f5c25eb231d39833dae57a8a81b1cd5b7d2a3176
kibukj <kibuzhj@gmail.com>
2022-05-02 18:02:34 +0000

    Add GitHub buttons

--- index.php
+++ index.php
@@ -7,6 +7,7 @@ session_start();
 		<meta name="viewport" content="width=device-width">
 		<title>pmms</title>
 		<script src="https://kit.fontawesome.com/5537a772c3.js" crossorigin="anonymous"></script>
+		<script async defer src="https://buttons.github.io/buttons.js"></script>
 		<script src="index.js"></script>
 		<link rel="stylesheet" href="index.css">
 	</head>
@@ -21,9 +22,16 @@ session_start();
 				<button type="button" id="lock-btn">
 					<i class="fas fa-lock-open"></i>
 				</button>
-				<input type="text" name="url" placeholder="Enter media URL...">
+				<input type="text" name="url" id="url" placeholder="Enter media URL...">
 				<button type="submit">&#xf35a;</button>
 			</form>
+			<div id="github-buttons">
+				<a class="github-button" href="https://github.com/kibook/pmms-web/subscription" data-icon="octicon-eye" data-size="large" data-show-count="true" aria-label="Watch kibook/pmms-web on GitHub">Watch</a>
+				<a class="github-button" href="https://github.com/kibook/pmms-web" data-icon="octicon-star" data-size="large" data-show-count="true" aria-label="Star kibook/pmms-web on GitHub">Star</a>
+				<a class="github-button" href="https://github.com/kibook/pmms-web/fork" data-icon="octicon-repo-forked" data-size="large" data-show-count="true" aria-label="Fork kibook/pmms-web on GitHub">Fork</a>
+				<a class="github-button" href="https://github.com/kibook/pmms-web/issues" data-icon="octicon-issue-opened" data-size="large" data-show-count="true" aria-label="Issue kibook/pmms-web on GitHub">Issue</a>
+				<a class="github-button" href="https://github.com/kibook/pmms-web/discussions" data-icon="octicon-comment-discussion" data-size="large" aria-label="Discuss kibook/pmms-web on GitHub">Discuss</a>
+			</div>
 		</div>
 	</body>
 </html>
